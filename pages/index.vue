55296370de6a5ed5ae4d8ec0dcbf3c6d481168be
Freddy <freddyamarante14@gmail.com>
2021-12-05 20:48:17 -0500

    Add example page

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,22 @@
   <div>
     <Navbar />
     <Table :type="available"/>
+
+    <div class="footer">
+      <el-button 
+        type="warning"
+        @click="$router.push('/example')"
+        >
+          Ir a ejemplo 2
+      </el-button>
+    </div>
   </div>
 </template>
 
-
+<style >
+  .footer {
+    position: absolute;
+    bottom: 30px;
+    width: 100%;
+  }
+</style>
